f1c8f43afedc071e233047b8b213be7be2aae8be
Accel1215 <WeWillAlter@mail.ru>
2021-04-25 01:38:49 +0300

    Make simple calc

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("HELLO");
+let first = Number(prompt("Input first number"));
+let sigh = prompt("Input sigh");
+let second = Number(prompt("Input second number"));
+
+if(sigh == "+") alert(first+second);
+else if(sigh == "-") alert(first - second);
+else if(sigh == "*") alert(first * second);
+else if(sigh == "/") alert(first / second);
+else if(sigh == "**") alert(first ** second);
+else if(sigh == "%") alert(first % second);
